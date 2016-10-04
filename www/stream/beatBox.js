a962014027f3aa2ac22d07d3740e17aef5e42986
David Bell-Brown <david.bellbrown@gmail.com>
2016-10-03 20:08:32 -0700

    nudge

--- www/stream/beatBox.js
+++ www/stream/beatBox.js
@@ -5,8 +5,8 @@ define(["./beat", "eventEmitter"],function(Beat, EventEmitter){
 		var prevBeat = null;
 		var that = this;
 		this.addBeat = function(){
+			//get that new beat, give it the old beat
 			//(beat() knows what to do with null)
-			//get that new beat
 			var beat = new Beat(prevBeat);
 			ee.emit("beat", beat);
 			console.log(beat.deltaTime());
