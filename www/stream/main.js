dd7aa639947ef39312a4c494e354affa3b2ae667
David Bell-Brown <david.bellbrown@gmail.com>
2016-10-03 20:36:11 -0700

    game display elements + re-focus on click

--- www/stream/main.js
+++ www/stream/main.js
@@ -2,22 +2,35 @@ define(function(require){
 	"use strict";
 	var Stats = require('./stats'),
 		BeatBox = require('./beatBox'),
-		Display = require('./display'),
+		// Graph = require('./graph'),
 		Ready = require('documentReady');
 
-	var display = new Display();
+	// var graph = new Graph();
 	var stats = new Stats();
 	var beatBox = new BeatBox();
+
 	//DOM interaction
-	var gameWindow = document.querySelector("#stream");
+	//graph container
+	var gameGraph = document.createElement("div");
+	gameGraph.setAttribute('id','gameWindow');
+	//hud container
+	var gameHud = document.createElement("div");
+	gameHud.setAttribute('id','gameHud');
 	//game focus
 	var focus = document.createElement("input");
 	focus.setAttribute('style', 'position:fixed;left:-100%;');
+
 	Ready(function(){
+		var gameContainer = document.querySelector("#stream")
 		console.log("ready!");
 		document.body.appendChild(focus);
+		gameContainer.appendChild(gameGraph);
+		gameContainer.appendChild(gameHud);
+		//focus
 		focus.focus();
-
+		gameGraph.addEventListener('click',function(e){
+			focus.focus();
+		});
 		//beat input
 		focus.addEventListener('keydown', beatBox.addBeat);
 	});
