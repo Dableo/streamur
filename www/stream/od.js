14e60b02aee8b3a4bdb7539fe33585a0de275a6e
David Bell-Brown <david.bellbrown@gmail.com>
2016-10-09 23:12:23 -0700

    added osu overall difficulty helpers

---
+++ www/stream/od.js
@@ -0,0 +1,12 @@
+define([],function(){
+	"use strict";
+	function OD(od) {
+		var goodInt = 6;
+		var badInt = 8;
+		var missInt = 10;
+		this.good = 79.5 - (goodInt * od);
+		this.bad = 139.5 - (badInt * od);
+		this.miss = 199.5 - (missInt * od);
+	}
+	return OD;
+});
--- www/stream/v_graph.js
+++ www/stream/v_graph.js
@@ -1,12 +1,13 @@
 //graph visual module, might use D3
-define(["d3"],function(d3){
+define(["d3","./od"],function(d3,OD){
 	"use strict";
 	function v_Graph(node) {
 		//mod for hitwindow, reflects osu settings
-		this.od = 9;
+		this.od = new OD(9);
 		var width = 1280;
 		var height = 720;
-		var margin = {top:24,right:24,bottom:24,left:48}
+		var y = d3.scale.linear()
+			.domain()
 		var svg = d3.select(node).append("svg")
 			.attr("viewBox", width/-2+" "+height/-2+" "+width+" "+height);
 		this.update = function(beat) {
